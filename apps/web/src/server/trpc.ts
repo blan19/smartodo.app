b3f038182e17891a490d384dca24f2aa339874e7
박준서 <oponize@naver.com>
2023-04-26 18:47:23 +0900

    feat(web): trpc 초기 세팅

---
+++ apps/web/src/server/trpc.ts
@@ -0,0 +1,18 @@
+import { initTRPC } from '@trpc/server';
+
+/**
+ * @description
+ *
+ * 컨텍스트는 모든 tRPC 프로시저가 액세스할 수 있는 데이터를 가지고 있습니다.
+ * 데이터베이스 연결 또는 인증 정보와 같은 것을 사용하기 위해 컨텍스트를 사용합니다.
+ *
+ * @ref
+ * https://trpc.io/docs/server/context
+ */
+
+const t = initTRPC.context().create();
+
+export const router = t.router;
+export const mergeRouters = t.mergeRouters;
+export const middleware = t.middleware;
+export const publicProcedure = t.procedure;
