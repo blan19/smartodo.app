a1f9d803f956208d710058d4a201cddcaf68b076
Junseo <66871265+blan19@users.noreply.github.com>
2023-04-26 16:01:53 +0900

    docs: Api와 관련된 코드 작성을 위한 주석 작성 (#2)
    * docs: api 코드 작성 컨벤션을 위한 주석 작성
    
    * docs: 빼먹은 주석 추가

---
+++ apps/web/src/http/endpoint/index.ts
@@ -0,0 +1,53 @@
+/**
+ * @description
+ *
+ * api 코드의 모듈화를 위한 폴더입니다.
+ *
+ * /endpoint/{endpoint}/index.ts
+ * apis,keys,mutations,queries를 named export
+ * ```ts
+ * export * from './apis'
+ * export * from './keys'
+ * export * from './mutations'
+ * export * from './queries'
+ * ```
+ *
+ * /endpoint/{endpoint}/keys.ts
+ * 뮤테이션,쿼리에 사용하는 키
+ * ```ts
+ * export const QUERY_KEYS = {
+ *   all: () => ["user"],
+ *   user: (id: number) => [...QUERY_KEYS.all, id],
+ * }
+ * ```
+ *
+ * /endpoint/{endpoint}/apis.ts
+ * 뮤테이션,쿼리에 사용하는 api 또는 개별적으로 사용하는 api
+ * ```ts
+ * export const getTodos = async () => {
+ *   return get(`/server/todos`)
+ * }
+ * ```
+ *
+ * /endpoint/{endpoint}/mutations.ts
+ * query state를 업데이트하는 코드
+ * ```ts
+ * export useDeleteTodo = () => {
+ *   return useMutation({
+ *     mutationFn: () => deleteTodo()
+ *   })
+ * }
+ * ```
+ *
+ * /endpoint/{endpoint}/queries.ts
+ * ```ts
+ * export useTodos = () => {
+ *   return useQuery({
+ *     queryKey: QUERY_KEYS.all(),
+ *     queryFn: () => getTodo()
+ *   })
+ * }
+ * ```
+ */
+
+export {};
